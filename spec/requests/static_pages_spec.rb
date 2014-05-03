d900fc73ff88573098a399ec9ccd8874fa3a6995
hphan62 <hphan62@yahoo.com>
2014-05-03 12:54:44 -0700

    Add Exercise 3.5 to static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
   describe "Home page" do
     
     it "should have the content 'Sample App'" do
@@ -11,7 +13,8 @@ describe "StaticPages" do
     
     it "should have the right title 'Home'" do
       visit '/static_pages/home'
-      page.should have_title("Ruby on Rails Tutorial Sample App | Home")
+      #page.should have_title("Ruby on Rails Tutorial Sample App | Home")
+      page.should have_title("#{base_title} | Home")
     end
   end
   
@@ -24,7 +27,7 @@ describe "StaticPages" do
     
     it "should have the right title 'Help'" do
       visit '/static_pages/help'
-      page.should have_title("Ruby on Rails Tutorial Sample App | Help")
+      page.should have_title("#{base_title} | Help")
     end
     
     
@@ -39,8 +42,25 @@ describe "StaticPages" do
     
     it "should have the right title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_title("Ruby on Rails Tutorial Sample App | About")
-      end
+      page.should have_title("#{base_title} | About")
+    end
+    
   end
+  
+  describe "Contract" do
+    
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    
+    it "should have the right title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_title("#{base_title} | Contact")
+    end    
+    
+  end
+  
+  
 end
 
